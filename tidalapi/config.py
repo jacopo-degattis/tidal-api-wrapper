dc61f7e3bea663c7642c0575e49fdeeda4a1d166
Jacopo De Gattis <jacopo.degattis@gmail.com>
2021-10-01 22:58:02 +0200

    added "BASE_API" and "BASE_LISTEN_API" to constants

--- tidalapi/config.py
+++ tidalapi/config.py
@@ -1,3 +1,5 @@
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:92.0) Gecko/20100101 Firefox/92.0"
 ORIGIN = "https://login.tidal.com"
 BASE_LOGIN_URI = "https://login.tidal.com"
+BASE_API = "https://api.tidal.com/v2"
+BASE_LISTEN_API = "https://listen.tidal.com/v1"
